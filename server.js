48092cb5f64854d3298ea1b2c304a260176cc00c
Yash Bhadane <yashbhadane483@gmail.com>
2026-03-08 15:21:46 +0530

    changes in json files

--- server.js
+++ server.js
@@ -53,8 +53,12 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(
-    `Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`,
-  ),
-);
+if (process.env.NODE_ENV !== "production" && !process.env.NETLIFY) {
+  app.listen(PORT, () =>
+    console.log(
+      `Server running in ${process.env.NODE_ENV || "development"} mode on port ${PORT}`,
+    ),
+  );
+}
+
+export default app;
